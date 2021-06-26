73ae3b206e56a7015364bc57a74c247a80e66510
shawlycode <>
2021-06-26 15:24:19 +0000

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbars from './Components/Navbar/Navbar';
+import Carousel from './Components/Carousel/Carousel';
+import Plan from './Components/Plans/Plan';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navbars />
+     <Carousel />
+     <Plan />
+     <Footer />
     </div>
   );
 }
